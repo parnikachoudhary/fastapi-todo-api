1aa3a7116776f450c107da470630f539702a5499
parnikachoudhary <parnika4apr@gmail.com>
2026-05-24 19:31:04 +0530

    Add bulk model and optional id

--- app/models.py
+++ app/models.py
@@ -1,9 +1,39 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
 
+# 1. Database table model
 class Todo(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str
     description: Optional[str] = None
     completed: bool = False
     priority: str = "medium"
+
+# 2. Create schema - POST body (no id is kept here)
+class TodoCreate(SQLModel):
+    title: str
+    description: Optional[str] = None
+    completed: bool = False
+    priority: str = "medium"
+
+# 3. Put schema - full replace
+class TodoPut(SQLModel):
+    title: str
+    description: Optional[str] = None
+    completed: bool
+    priority: str
+
+# 4. Patch schema - partial update (all fields are optional here)
+class TodoUpdate(SQLModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    completed: Optional[bool] = None
+    priority: Optional[str] = None
+
+# 5. Bulk update (id is must)
+class TodoBulkUpdateItem(SQLModel):
+    id: int
+    title: Optional[str] = None
+    description: Optional[str] = None
+    completed: Optional[bool] = None
+    priority: Optional[str] = None
--- app/routes/todo.py
+++ app/routes/todo.py
@@ -1,14 +1,37 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Body
 from sqlmodel import Session
 from app.database import get_session
-from app.models import Todo
-from app.crud import create_todo, get_todos, get_todo, update_todo, delete_todo
+from app.models import Todo, TodoCreate, TodoPut, TodoBulkUpdateItem, TodoUpdate
+from app.crud import (
+    create_todo, 
+    get_todos, 
+    get_todo, 
+    delete_todo, 
+    patch_todo, 
+    put_todo,
+    bulk_create_todos,
+    bulk_delete_todos,
+    bulk_update_todos
+    ) 
 
 router = APIRouter()
 
-@router.post("/")
-def add_todo(todo: Todo, session: Session = Depends(get_session)):
-    return create_todo(session, todo)
+
+# BULK UPDATE
+@router.patch("/bulk")
+def update_bulk_todos(items: list[TodoBulkUpdateItem], session: Session = Depends(get_session)):
+    return bulk_update_todos(session, items)
+
+# BULK DELETE
+@router.delete("/bulk")
+def delete_bulk_todos(ids: list[int] = Body(...), session: Session = Depends(get_session)):
+    return bulk_delete_todos(session, ids)
+
+# BULK ADD
+@router.post("/bulk")
+def add_bulk_todos(todos: list[TodoCreate], session: Session = Depends(get_session)):
+    return bulk_create_todos(session, todos)
+
 
 @router.get("/")
 def read_todos(session: Session = Depends(get_session)):
@@ -19,14 +42,27 @@ def read_todos(session: Session = Depends(get_session)):
 def read_todo(todo_id: int, session: Session = Depends(get_session)):
     return get_todo(session, todo_id)
 
+
+@router.post("/")
+def add_todo(todo: TodoCreate, session: Session = Depends(get_session)):
+    return create_todo(session, todo)
+
 @router.put("/{todo_id}")
-def update_todo_endpoint(todo_id: int, updated_todo: Todo, 
-                         session: Session = Depends(get_session)):
-    todo = update_todo(session, todo_id, updated_todo)
+def replace_todo(todo_id: int, updated_todo: TodoPut, session: Session = Depends(get_session)):
+    todo = put_todo(session, todo_id, updated_todo)
+    if not todo:
+        return {"error": "Todo not found"}
+    return todo
+
+@router.patch("/{todo_id}")
+def partial_update_todo(todo_id: int, updated_todo: TodoUpdate, session: Session = Depends(get_session)):
+    todo = patch_todo(session, todo_id, updated_todo)
     if not todo:
         return {"error": "Todo not found"}
     return todo
 
 @router.delete("/{todo_id}")
-def delete_todo_endpoint(todo_id: int, session: Session = Depends(get_session)):
+def remove_todo(todo_id: int, session: Session = Depends(get_session)):
     return delete_todo(todo_id, session)
+
+
